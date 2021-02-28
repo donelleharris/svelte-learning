b5ae89f5a0b8b1fe5a07fd33bb08059815c608c7
Donelle Harris <donelle.harris@gmail.com>
2021-02-28 08:56:37 -0600

    Initialize files for Udemy Svelte.js course
    Initial files created
    Course content through #16 completed

---
+++ src/App.svelte
@@ -0,0 +1,25 @@
+<script>
+import ContactCard from "./ContactCard.svelte";
+
+	let name = "Donelle";
+	let age = 52;
+	function incrementAge(){
+		age += 1;
+	}
+	function changeName(){
+		name = "Marie";
+	}
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
+
+<h1>Hello {name}, my age is {age}!</h1>
+<button on:click="{incrementAge}">Change Age</button>
+<button on:click="{changeName}">Change Name</button>
+<input type="text" bind:value="{name}">
+
+<ContactCard />
